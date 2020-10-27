582f3aa1578b72e902d0771559c620878f176180
tsubasakarasawa <zm19092@josai.ac.jp>
2020-10-27 02:06:24 +0000

    1027

--- ex6-5.js
+++ ex6-5.js
@@ -1,9 +1,17 @@
-var who65 = "world";
+var who65 = 'world';
+
 function sayhello65() {
+
     alert('Hello, ' + who65 + '!');
+
 }
 function taro65() {who = '太郎';
 }
+
 function hanako65() {
+
     who = '花子';
+}
+function gakita() {
+    alert(花子+太郎);
 }
