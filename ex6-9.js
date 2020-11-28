9a85de449ed865b5df053fd0d97e38ace6f4c6ad
tsubasakarasawa <zm19092@josai.ac.jp>
2020-11-28 08:26:55 +0000

    1128

--- ex6-9.js
+++ ex6-9.js
@@ -1,6 +1,7 @@
 function txtchange1() {
     document.getElementById('txt1').innerHTML= 'ナンバーワン!';
 }
+
 function imgchange1() {
-  document.getElementById('logo').src = './image/Jenco.jpg';
+  document.getElementById('logo').src = './image/keiei_small.jpg';
 }
