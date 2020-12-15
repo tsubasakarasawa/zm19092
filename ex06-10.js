8075f0fce20e657e9dfb89664e141517677b92e7
tsubasakarasawa <zm19092@josai.ac.jp>
2020-12-15 01:53:58 +0000

    1215

--- ex06-10.js
+++ ex06-10.js
@@ -2,4 +2,7 @@ function imgchange061() {
     document.getElementById("logo061").src=
      "./image/edokato.jpg";
     }
-    
+    function imgchange061() {
+    document.getElementById("logo061").src=
+     "./image/kanekin.jpg";
+    }
