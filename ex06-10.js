2ec0b93cf4f29a4c9b31ee9047b9990d508eda02
tsubasakarasawa <zm19092@josai.ac.jp>
2020-12-15 01:35:40 +0000

    1215

--- ex06-10.js
+++ ex06-10.js
@@ -2,5 +2,5 @@ function imgchange061() {
     document.getElementById("logo061").src=
      "./image/edokato.jpg";
     }
-    <img id="gazo" onclick="changeIMG()" src="image/edikato.jpg"
+    <img id="gazo" onclick="changeIMG()" src="./image/edikato.jpg"
    width="320" height="240" border="0" alt=""></img>
